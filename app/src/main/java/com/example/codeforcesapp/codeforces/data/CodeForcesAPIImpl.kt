7a7f39d0b9d100ee8a051298c8c02d2d3e634f36
Muhammed Nashaat <muhammednashaat@gmail.com>
2025-10-25 23:43:42 +0300

    adding getContestList API call

--- app/src/main/java/com/example/codeforcesapp/codeforces/data/CodeForcesAPIImpl.kt
+++ app/src/main/java/com/example/codeforcesapp/codeforces/data/CodeForcesAPIImpl.kt
@@ -1,9 +1,13 @@
 package com.example.codeforcesapp.codeforces.data
 
+import com.example.codeforcesapp.codeforces.data.mappers.toContest
 import com.example.codeforcesapp.codeforces.data.mappers.toUser
+import com.example.codeforcesapp.codeforces.data.networking.dto.ContestDto
+import com.example.codeforcesapp.codeforces.data.networking.dto.ContestResponseDto
 import com.example.codeforcesapp.codeforces.data.networking.dto.UserDto
 import com.example.codeforcesapp.codeforces.data.networking.dto.UserResponseDto
 import com.example.codeforcesapp.codeforces.domain.CodeForcesAPI
+import com.example.codeforcesapp.codeforces.domain.Contest
 import com.example.codeforcesapp.codeforces.domain.User
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
@@ -26,5 +30,16 @@ class CodeForcesAPIImpl(
         val userDto:UserDto = userResponseDto.result[0]
         val user:User = userDto.toUser()
         return user
+    }
+
+    /////////////////////////////////////////////////////////////////
+    override suspend fun getContestList(): List<Contest> {
+        val result: HttpResponse = httpClient.get(
+            urlString = "https://codeforces.com/api/contest.list"
+        )
+        val responseDto: ContestResponseDto = result.body()
+        val contestDtoList : List<ContestDto> = responseDto.result
+        val contestList: List<Contest> = contestDtoList.map { it.toContest() }
+        return contestList
     }
 }
--- app/src/main/java/com/example/codeforcesapp/codeforces/domain/CodeForcesAPI.kt
+++ app/src/main/java/com/example/codeforcesapp/codeforces/domain/CodeForcesAPI.kt
@@ -2,4 +2,5 @@ package com.example.codeforcesapp.codeforces.domain
 
 interface CodeForcesAPI {
     suspend fun getUser(handles: String):User
+    suspend fun getContestList():List<Contest>
 }

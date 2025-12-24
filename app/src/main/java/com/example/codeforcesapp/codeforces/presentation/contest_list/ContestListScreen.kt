ff75d17f33e25b0321843bc8754a780586b8846f
Muhammed Nashaat <muhammednashaat@gmail.com>
2025-12-24 03:14:27 +0200

    update: ContestList Screen

--- app/src/main/java/com/example/codeforcesapp/codeforces/presentation/contest_list/ContestListScreen.kt
+++ app/src/main/java/com/example/codeforcesapp/codeforces/presentation/contest_list/ContestListScreen.kt
@@ -1,10 +1,23 @@
 package com.example.codeforcesapp.codeforces.presentation.contest_list
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import com.example.codeforcesapp.codeforces.presentation.models.ContestUi
 
@@ -13,15 +26,20 @@ fun ContestListScreen(
     contestUiList: List<ContestUi>,
     modifier: Modifier
 ){
-    LazyColumn(
-        verticalArrangement = Arrangement.spacedBy(5.dp),
+    Column(
         modifier = modifier
     ) {
-        items(contestUiList){ contestUi ->
-            ContestListItem(
-                contestUi = contestUi,
-                modifier = Modifier
-            )
+        ContestListHeader()
+        Spacer(modifier = Modifier.height(10.dp))
+        LazyColumn(
+            verticalArrangement = Arrangement.spacedBy(5.dp),
+        ) {
+            items(contestUiList){ contestUi ->
+                ContestListItem(
+                    contestUi = contestUi,
+                    modifier = Modifier
+                )
+            }
         }
     }
 }

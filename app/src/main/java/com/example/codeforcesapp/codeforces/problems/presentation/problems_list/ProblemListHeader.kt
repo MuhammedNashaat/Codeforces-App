1516434f32eee473faeb55acf3e3556ebf65b986
Muhammed Nashaat <muhammednashaat@gmail.com>
2026-03-03 01:32:04 +0200

    add: ProblemListHeader

---
+++ app/src/main/java/com/example/codeforcesapp/codeforces/problems/presentation/problems_list/ProblemListHeader.kt
@@ -0,0 +1,41 @@
+package com.example.codeforcesapp.codeforces.problems.presentation.problems_list
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ProblemListHeader(){
+    Row (
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(10.dp),
+        modifier = Modifier.fillMaxWidth()
+            .clip(RoundedCornerShape(20.dp))
+            .background(MaterialTheme.colorScheme.primaryContainer)
+            .padding(top = 5.dp, bottom = 5.dp)
+    ){
+        Text(
+            text = "Problem Name",
+            color = MaterialTheme.colorScheme.onPrimaryContainer,
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.padding(start = 10.dp)
+                .weight(1f)
+        )
+        Text(
+            text = "Rating",
+            color = MaterialTheme.colorScheme.onPrimaryContainer,
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.padding(end = 10.dp)
+        )
+    }
+}
--- app/src/main/java/com/example/codeforcesapp/codeforces/problems/presentation/problems_list/ProblemListScreen.kt
+++ app/src/main/java/com/example/codeforcesapp/codeforces/problems/presentation/problems_list/ProblemListScreen.kt
@@ -3,7 +3,9 @@ package com.example.codeforcesapp.codeforces.problems.presentation.problems_list
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
@@ -29,6 +31,8 @@ fun ProblemListScreen(
         Column(
             modifier = modifier
         ) {
+            ProblemListHeader()
+            Spacer(modifier = Modifier.height(10.dp))
             LazyColumn(
                 verticalArrangement = Arrangement.spacedBy(5.dp)
             ) {
